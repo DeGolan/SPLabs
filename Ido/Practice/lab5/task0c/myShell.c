d12ba6f79d6bbabc7eaf3c797f6ac0d1ea7e9347
Ido <idocarmi2205@gmail.com>
2021-05-02 16:44:11 +0300

    finished 0c

--- Ido/Practice/lab5/task0c/myShell.c
+++ Ido/Practice/lab5/task0c/myShell.c
@@ -3,20 +3,50 @@
 #include <stdio.h>
 #include <string.h>
 #include "LineParser.h"
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define BUF_SIZE 2048
+int debug = 0;
 
 void execute(cmdLine *pCmdLine)
 {
-    if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
+    pid_t pid = fork();
+    int* statLoc;
+    if (pid == -1)
     {
-        perror("Error");
+        perror("Could not fork");
         _exit(1);
+    }
+    if (pid == 0)
+    {
+        if (debug)
+        {
+            fprintf(stderr, "PID: %d\n", getpid());
+            fprintf(stderr, "Executing command: %s\n", pCmdLine->arguments[0]);
+        }
+        if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1)
+        {
+            perror("Error");
+            _exit(1);
+        }
+    }
+    else
+    {
+        waitpid(pid,statLoc,WEXITED);
     }
 }
 
 int main(int argc, char **argv)
 {
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-d") == 0)
+        { //debug mode
+            debug = 1;
+            fprintf(stderr, "-d\n");
+        }
+    }
     char workingDir[PATH_MAX];
     char buff[BUF_SIZE];
     cmdLine *line;
