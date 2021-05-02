e1b6f461d7386f472ec43c38ba675726baeb859d
DeGolan <omergo66@gmail.com>
2021-05-02 10:25:28 +0300

    lab5 pract start

---
+++ Omer/Practice/prac/convertor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main (int argc, char** argv){
+    
+    int counter1=0;
+    char input[30];
+    int output[30];
+
+    fgets(input,30,stdin);
+    puts(output);
+
+    for (size_t i = 0; input[i] != 0; i=i+3)
+    {
+        
+       
+        
+
+
+    }
+    
+    
+    
+    
+    
+    
+    printf("hello");
+
+    return 0;
+}
