9dc2c737eb79dbcd06f725654a3a2a54a30b3007
DeGolan <omergo66@gmail.com>
2021-05-09 09:34:37 +0300

    merge from ido

--- Assignments/Assignment2/calc.c
+++ Assignments/Assignment2/calc.c
@@ -1,25 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-// A structure to represent a stack
-typedef struct Stack
-{
-    int top;
-    unsigned capacity;
-    int *array;
-} Stack;
-
-// function to create a stack of given capacity. It initializes size of
-// stack as 0
-struct Stack *createStack(unsigned capacity)
-{
-    struct Stack *stack = (struct Stack *)malloc(sizeof(struct Stack));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->array = (int *)malloc(stack->capacity * sizeof(int));
-    return stack;
-=======
 char* toShorterStr(char* str,int size,int leadingZeroes){
     //printf("SIZE: %d\n",size);
     char* newResult=NULL;
@@ -37,7 +18,6 @@ char* toShorterStr(char* str,int size,int leadingZeroes){
     free(str);
     return newResult;
 
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
 }
 
 char* octOperation(char* num1,char* num2,char* op){
@@ -63,53 +43,6 @@ char* octOperation(char* num1,char* num2,char* op){
     //printf("%d,%d,%d\n",size1,size2,maxSize);
     int digit1,digit2,resultDigit,carry=0;
 
-<<<<<<< HEAD
-// Function to return the top from stack without removing it
-int peek(struct Stack *stack)
-{
-    if (isEmpty(stack))
-        return -1;
-    return stack->array[stack->top];
-}
-
-char* octAdd(char* num1,char* num2){
-    int size1,size2,maxSize;
-    //get the size of each number
-    for(size1=0; num1[size1]!=0;size1++);
-    //printf("Size of first number: %d\n",size1);
-    
-    for(size2=0; num2[size2]!=0;size2++);
-    //printf("Size of second number: %d\n",size2);
-    
-    maxSize=(size1>size2)?size1:size2;
-
-    size1-=1;//index to the msb
-    size2-=1;//index to the msb
-    //printf("Max Size: %d\n",maxSize+1);
-
-    char* result=(char*)malloc(maxSize+2);
-    result[maxSize+2]=0;//NULL TERMINATED STRING
-
-    //result[0]=0;//to know if we used the extra bit in case of carry
-    int digit1,digit2,resultDigit,carry=0;
-    while(maxSize>=0){
-        if(size1>=0){//in case the other number has more digits
-            digit1=num1[size1]-'0';//convert from char to oct digit
-        }
-        else{
-            digit1=0;
-        }
-       // printf("digit1: %d\n",digit1);
-        if(size2>=0){
-             digit2=num2[size2]-'0';//convert from char to oct digit
-        }
-        else{//in case the other number has more digits
-            digit2=0;
-        }
-        //printf("digit2: %d\n",digit2);
-        
-        resultDigit=digit1+digit2+carry;//the main add
-=======
     while(maxSize>=0){
         
         if(size1>=0){//in case the other number has more digits
@@ -139,7 +72,6 @@ char* octAdd(char* num1,char* num2){
             resultDigit=digit1&digit2;
         }
         
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
         //printf("result digit before carry: %d\n",resultDigit);
         if(resultDigit>7){//in case of carry
             resultDigit-=8;
@@ -149,27 +81,12 @@ char* octAdd(char* num1,char* num2){
             carry=0;
         }
          //("result digit after carry: %d\ncarry: %d\n",resultDigit,carry);
-<<<<<<< HEAD
-        
-
-=======
       
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
         result[maxSize]=resultDigit+'0';//convert from oct to char and put int the result string
 
         maxSize--;
         size1--;
         size2--;
-<<<<<<< HEAD
-    }
-
-    if(result[0]=='0'){
-        //The lsb has not been used
-        //printf("The lsb has not been used\n");
-        result=result+1;
-    }
-
-=======
     }
     int leadingZeroes=0;
     while(result[leadingZeroes]=='0'){
@@ -179,37 +96,22 @@ char* octAdd(char* num1,char* num2){
         result=toShorterStr(result,resultSize+1-leadingZeroes,leadingZeroes);
     }
 
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
     return result;
     
 }
 
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
-    do{
-        char num1[80];
-        char num2[80];
-=======
     
     do{
         char num1[80];
         char num2[80];
         char op[2];
         op[2]=0;
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
         printf("Calc: ");
         gets(num1);
         printf("Cals: ");
         gets(num2);
-<<<<<<< HEAD
-        printf("ADD Result: %s\n",octAdd(num1,num2));
-
-    }while(1);
-    
-
-
-=======
         printf("Enter op: ");
         gets(op);
         if(op[0]=='q')break;
@@ -226,7 +128,6 @@ int main(int argc, char **argv)
     
 
 
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
     /*
     Stack *stack = createStack(63);
     stack->capacity = 5;
--- Assignments/Assignment2/calculator.s
+++ Assignments/Assignment2/calculator.s
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-section	.rodata	
-    calc_string : db "calc:" , 0
-    print_string_format: db "%s",10,0	; format string
-
-
-
-section .bss
-    cap : resb 4 ;stack capacity
-    stck: resb 4 ;number of objects in stack
-    input: resb 80 ;input buffer
-    pointer: resb 4 ;will hold pointer to node
-
-
-section .text
-    align 16
-    global main
-=======
 %macro printString 1
     pushad
     push %1
@@ -100,7 +82,6 @@ section .bss
    
 section .text
     align 16
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
     extern printf
     extern fprintf 
     extern fflush
@@ -113,121 +94,6 @@ section .text
     extern stdout
     extern stdin
     extern stderr
-<<<<<<< HEAD
-
-main:
-    
-    mov dword [stck], 0
-    mov dword [cap], 5 ; default
-
-    cmp dword [esp+4], 1 ;check if the user entered capacity
-    je mainLoop
-
-    mov eax, [esp+8] ;move args to eax
-    mov eax, [eax+4] ;save first argument to eax
-    mov dword[cap], eax ;save capacity
-    
-mainLoop:
-    ;first thing in main loop is print "calc:" and get input
-    printCalc:
-        push calc_string ;print calc
-        push print_string_format
-        call printf
-        add esp, 8
-        push dword [stdin] ;geting input
-        push 80 
-        push input
-        call fgets
-        add esp, 12
-        
-        mov esi, 0; will point to the last digit of input
-    inputSize:
-        add esi,1
-        mov byte AL,[input+esi]
-        cmp byte AL,0
-        jne inputSize
-        sub esi,1 ; remove one to point at the last digit and not 0
-
-    ;start creating list to input to stack
-    initList:
-        mov ecx, 7 ; node size (3 data, 4 next node)
-        pushad
-        push ecx
-        call calloc ;eax will hold pointer to the new node
-        add esp, 4
-        mov [pointer],eax
-        popad
-        mov eax,[pointer]
-
-
-        mov dword ebx, 1 ;bit index
-        mov byte CH, 1  ;bit checker
-    ;insert data to node    
-    insertData:
-        cmp byte CH,8 ;we read 3 bits
-        je reset
-        cmp esi,0 ;finshed to read number
-        je  fin
-        sub esi,1 ;move to next digit
-    cont1:
-        mov byte DL, CH
-        mov CL,[input+esi]
-        sub CL,'0'  ;convert from char to oct digit
-        AND DL, CL ;check if the wanted bit is on
-        cmp byte DL, CH  ;if so, add to the data the nubmer
-        jne nextBit
-        add dword [eax+ebx],ebx ;add to the data the number
-        
-    nextBit:
-       
-        shl CH,1
-        shl ebx,1
-        cmp dword ebx,16777216 ;if the 25'th bit is on
-        jne insertData
-        ;new link
-        mov edx, eax; save the head
-        mov ecx, 7            
-        pushad
-        push ecx
-        call calloc; ;eax will hold pointer to the new node
-        add esp,4
-        mov [pointer],eax
-        popad
-        mov eax,[pointer] ;eax=curr
-        mov [edx+3],eax   ;pointing to the new node
-
-
-     reset:
-        mov dword ebx, 1 ;bit index
-        mov byte CH, 1  ;bit checker
-        jmp insertData
-      
-    fin:
-        mov dword [edx+3], 0 ;NULL
-
-        ret
-
-        
-
-
-
-                                        
-
-    
-
-
-
-
-
-       
-
-
-
-
-
-
-    
-=======
     global main
 
 main:
@@ -573,4 +439,3 @@ freePointer:
         add esp,4
         popad
         ret
->>>>>>> 9a2d4b87f451ea326b8e24e621144cc5b43a051d
